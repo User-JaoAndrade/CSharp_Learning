a31dd85123913203a67ef877dff7b43ef06bedb2
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-19 17:31:41 -0300

    Add EX08

---
+++ Estrutura_Sequencial/08_CalculandoSalario/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês.*/
+
+using System;
+
+namespace _08_CalculandoSalario{
+    class Program{
+        static void Main()
+        {
+            Console.Write("Quanto você recebe por hora?\n-> ");
+            float ganho_por_hora = float.Parse(Console.ReadLine());
+
+            Console.Write("Quantas horas trabalhou esse mês?\n-> ");
+            int horas_trabalhadas = int.Parse(Console.ReadLine());
+
+            Console.Write($"Seu salário vai ser de {ganho_por_hora * horas_trabalhadas}");
+        }
+    }
+}
