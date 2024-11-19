48eeea8e835515b1ee0497a1183b4f0e0ae6e3b3
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-19 17:18:34 -0300

    Add Ex07

---
+++ Estrutura_Sequencial/07_Dobrando_a_Area_de_um_quadrado/Program.cs
@@ -0,0 +1,17 @@
+﻿// Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+using System;
+
+namespace _07_Dobrando_a_Area_de_um_quadrado{
+    class Program{
+        static void Main()
+        {
+            Console.Write("Informe o comprimento do lado do quadrado: ");
+            float sqr_lado = float.Parse(Console.ReadLine());  
+
+            float sqr_area = (float)Math.Pow(sqr_lado, 2); // Calculando a área
+
+            Console.Write($"\nO Dobro da Área do quadrado é {sqr_area * 2}");
+        }
+    }
+}
